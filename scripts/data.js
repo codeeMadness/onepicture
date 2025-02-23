3155bebfb4d532a7c9422f3293067397c44052ce
codeeMadness <codee.madness@gmail.com>
2025-02-23 14:09:57 +0700

    update

--- scripts/data.js
+++ scripts/data.js
@@ -1,6 +1,6 @@
 export const java_arr = ['access_modifiers2','ArrayList','Custom_Annotations','equals','Exception','garbage_collection','How Java Compile','numeric_promotion','operator_precedence','order_execution','overloading_methods','Spring_Framework','Var']
 
-export const system_design_arr = ['jwt_workflow','SAML_workflows','SOLID','OOP']
+export const system_design_arr = ['jwt_workflow','SAML_workflows','SOLID','OOP','ACID']
 
 export const mixed = () => {
     const arr = java_arr.concat(system_design_arr);
