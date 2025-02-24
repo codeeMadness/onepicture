2ebde53ad7153154eb0722ba67b8af7b560894e0
codeeMadness <codee.madness@gmail.com>
2025-02-24 22:01:24 +0700

    update

--- scripts/script.js
+++ scripts/script.js
@@ -2,29 +2,38 @@ import { java_arr, mixed, system_design_arr } from './data.js';
 import { Modal } from './Modal.js';
 import { Music } from './Music.js';
 import { Slide } from './Slide.js';
+import { Thumbnail } from './Thumbnail.js';
+import { ViewMode } from './ViewMode.js';
 
 //default Slide
 const mixedTopic = new Slide(mixed());
 mixedTopic.createSlides();
 mixedTopic.showSlides();
+const mixedThumbnail = new Thumbnail(mixed());
+mixedThumbnail.generate();
 const pickMixed = document.getElementById("mixed-topics");
 pickMixed.addEventListener('click', () => {
     mixedTopic.createSlides();
     mixedTopic.showSlides();
+    mixedThumbnail.generate();
 });
 
 const javaTopic = new Slide(java_arr);
+const javaThumbnail = new Thumbnail(java_arr);
 const pickJava = document.getElementById("java");
 pickJava.addEventListener('click', () => {
     javaTopic.createSlides();
     javaTopic.showSlides();
+    javaThumbnail.generate();
 });
 
 const systemTopic = new Slide(system_design_arr);
+const systemThumbnail = new Thumbnail(system_design_arr);
 const pickSystemDesign = document.getElementById("system-designs");
 pickSystemDesign.addEventListener('click', () => {
     systemTopic.createSlides();
     systemTopic.showSlides();
+    systemThumbnail.generate();
 });
 
 
@@ -36,6 +45,9 @@ modal.action();
 
 const infoModal = new Modal("myInfo", document.querySelector('.info'));
 infoModal.action();
+
+const viewMode = new ViewMode();
+viewMode.toggleViewMode();
 
 //copyright
 document.getElementById('currentYear').textContent = new Date().getFullYear();
