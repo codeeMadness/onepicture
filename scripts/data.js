7eeda607bf1e03066500fe4dbb217228b2482b4c
codeeMadness <codee.madness@gmail.com>
2025-02-25 22:28:32 +0700

    refactor img name

--- scripts/data.js
+++ scripts/data.js
@@ -1,6 +1,6 @@
-export const java_arr = ['access_modifiers2','ArrayList','Custom_Annotations','equals','Exception','garbage_collection','How Java Compile','numeric_promotion','operator_precedence','order_execution','overloading_methods','Spring_Framework','Var']
+export const java_arr = ['Access Modifiers','ArrayList','Custom Annotations','Equals','Exception','Garbage Collection','How Java Compile','Numeric Promotion','Operator Precedence','Order Execution','Overloading Methods','Spring','Var']
 
-export const system_design_arr = ['jwt_workflow','SAML_workflows','SOLID','OOP','ACID']
+export const system_design_arr = ['JWT Workflow','SAML Workflow','SOLID','OOP','ACID']
 
 export const mixed = () => {
     const arr = java_arr.concat(system_design_arr);
